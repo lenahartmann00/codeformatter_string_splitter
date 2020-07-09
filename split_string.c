887638a6c0294cc0ff81aecd0edcf357847a67f3
Lena Hartmann <lena.hartmann00@googlemail.com>
2020-07-09 18:02:22 +0200

    Update split_string.c

--- split_string.c
+++ split_string.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-const int SPLIT_AT = 30; // at which character should the string be splitted
+const int SPLIT_AT = 30; // length of the sepearted strings (split after the 30th character)
 
-int main (void) {
-    char test [] = "PLEASE ENTER YOUR STRING HERE! IT WILL BE SPLITTED";
+int main (int argc, char * argv[]) {
+    
+    if(argc != 2) {
+        printf("Usage: ./split_string \"The string you would like to split\"\n");
+        return 1;
+    }
+
+    char *user_text = argv[1];
+
     int counter = 0;
     printf("\"");
-    for(int i = 0, n = strlen(test); i < n; i++) {
-        if(counter == SPLIT_AT) {
+    for(int i = 0, n = strlen(user_text); i < n; i++) {
+        if(counter == SPLIT_AT - 1) {
             counter = 0;
             printf("\"+\n\"");
         }
-        printf("%c", test[i]);
+        printf("%c", user_text[i]);
         counter++;
     }
     printf("\"\n");
